b149ca473afa980f45661ecdef7b45d7e02a8a7d
ThatCrazyAlpacaGuy <65831091+ThatCrazyAlpacaGuy@users.noreply.github.com>
2020-05-23 16:48:41 -0600

    Add files via upload

--- Python-Lib/transistors.py
+++ Python-Lib/transistors.py
@@ -1,35 +1,41 @@
 class transistor:
-    #sets the state to zero by default
-    state = 0
+    
+    statebool = False
 
     #constructer
-
-
-    #sets the state
-    def setstate(self, newstate):
-        if newstate == 1 : self.state = 1
-        else: self.state = 0
-
-    #gets the state
-    def getstate(self, asstring):
-        if asstring == True:
-            return str(self.state)
+    def __init__(self, state):
+        if state >= 1:
+            self.statebool = True
         else:
-            return self.state
-
-    #sets the output
-    def out(self, trans):
-        if self.state == 1: trans.setstate(1)
-        else: trans.setstate(0)
+            self.statebool = False
     
-    #checks if on
-    def ifon(self):
-        if self.state == 1: return True
-        else: return False    
-    def invert(self):
-        if self.state == 1:
-            self.state = 0
+    #getting the state
+    def getstate(self):
+        if self.statebool == True:
+            return 1
         else:
-            self.state = 1
+            return  0
+    
+    #setting the state
+    def setstate(self, state):
+        if state >= 1:
+            self.statebool == True 
+        else:
+            self.statebool == False
+
+    #seting the output
+    def out(self, transistor):
+        if self.statebool == True:
+            transistor.setstate(1)
+        else:
+            transistor.setstate(0)
+    
+    #if on bool
+    def ifon(self):
+        return self.statebool
+
+    
+
+
 
 
